311a6d5a354fb57dfb61c4613283c9228513143e
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-27 12:30:15 +0200

    Add TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
-
 function App() {
 
   return (
-    <div>
-        Hello World !
+    <div className="bg-green-700 text-white p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
     </div>
   )
 }
