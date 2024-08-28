092a0d8fcec4e178c46fc9027e48d1fc27d5bdd0
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-28 20:35:13 +0200

    Add module Step 3 Pick Add Ons

---
+++ src/layouts/Form/FormAdds.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useForm, FormProvider } from "react-hook-form";
+
+export default function FormAdds({ onNext }) {
+  return (
+    <div className="col-span-2 p-6 sm:p-12">
+            <div className="pb-8">
+                <div className="block text-2xl text-sky-950 font-semibold">
+                    Pick Add-Ons
+                </div>
+                <div className="block text-md font-normal text-gray-400">
+                    Enhance your gaming experience with add-ons.
+                </div>
+            </div>
+            <form >
+                Formulaire à compléter
+            
+                <div className="flex justify-between mt-6">
+                    <button type="button" 
+                        className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-xs font-medium text-sky-900 bg-gray-200 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-950">
+                        Previous
+                    </button>
+                    <button type="submit" 
+                        className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-xs font-medium text-white bg-sky-950 hover:bg-sky-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-950">
+                        Next Step
+                    </button>
+                </div>
+            </form>
+        </div>
+  )
+}
